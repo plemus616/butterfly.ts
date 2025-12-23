0b365fd3966d26cc617c91d1df96c5046ac2d553
plemus616 <plemus@rototec.com.gt>
2025-12-23 13:40:29 -0600

    endpoint para registros de vendedores

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import Fastify from 'fastify';
 import {routes} from "./routes/routes.ts";
 import fastifyWebsocket from "@fastify/websocket";
+import fastifyCors from "@fastify/cors";
 import {wsListenerService} from "./services/ws-listener-services.ts";
 const app = Fastify({logger:true});
-await app.register(fastifyWebsocket);
+await app.register(fastifyCors, {
+   origin: '*',
+   methods: ["GET", "POST", "OPTIONS"]
+});
+await app.register(fastifyWebsocket, {
+    options: {
+        verifyClient: () => true
+    }
+});
 await app.register(routes, {prefix: "/api"});
 await wsListenerService();
 app.listen({port:6666, host:'0.0.0.0'}, (err)=>{
